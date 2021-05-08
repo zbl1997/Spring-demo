1834a47027716bdfe3cd80fc0b7823b93af3fb75
zbl1997 <aA2311109>
2021-05-08 17:40:41 +0800

    RabbitMQ

--- src/main/java/com/zbl/springdemo/controller/mq/SimpleMqController.java
+++ src/main/java/com/zbl/springdemo/controller/mq/SimpleMqController.java
@@ -24,6 +24,7 @@ public class SimpleMqController {
     public void helloMQ() {
         MessageProperties messageProperties = new MessageProperties();
         messageProperties.setDeliveryMode(MessageDeliveryMode.PERSISTENT);//消息持久化
+        messageProperties.setExpiration("6000");// 消息过期时间，当消息发送时发现过期了，会发送到死信或删除。想让它过期就删，需要装插件 https://www.rabbitmq.com/community-plugins.html
 
         Message message = new Message("hello".getBytes(), messageProperties);
         rabbitTemplate.convertAndSend("simpleQueue",message);
